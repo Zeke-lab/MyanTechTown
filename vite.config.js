ba6292787371d3f74f2f53385efe6c27a2bf519c
Zeke-lab <yemyatmin192@gmail.com>
2026-01-08 01:33:56 +0700

    Configure Vite base URL and include dist folder for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  base: "/MyanTechTown/", // This is crucial for GitHub Pages deployment
   plugins: [react()],
   server: {
     watch: {
