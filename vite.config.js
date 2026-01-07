413d48fabf81ad20c8867dbdaa3e9caa4d795569
Zeke-lab <yemyatmin192@gmail.com>
2026-01-08 02:38:24 +0700

    Configure Vite build script for relative paths on GitHub Pages and remove base from vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/MyanTechTown/', // Set base path for GitHub Pages
   plugins: [react()],
   build: {
     outDir: 'dist',
